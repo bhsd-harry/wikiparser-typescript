aad2fdc04c0f1f45cc5205003f1c574388364b00
bhsd <2545473905@qq.com>
2023-04-16 14:45:39 -0700

    feat(diff.sh): improve

--- diff.sh
+++ diff.sh
@@ -2,9 +2,12 @@ ext=${1:$(( ${#1} - 5 ))}
 if [[ $ext == '.json' ]] || [[ $ext == '.d.ts' ]]
 then
 	js=$1
-	src=$1
+	git diff --no-index --ignore-all-space --color-moved $1 ../wikiparser-node/$js
 else
-	js=${1:0:$(( ${#1} - 2 ))}js
-	src=dist/$js
+	js=${1:0:$(( ${#1} - 2 ))}
+	git diff --no-index --ignore-all-space --color-moved --exit-code dist/${js}js ../wikiparser-node/${js}js
+	if [[ $? -eq 0 ]]
+	then
+		git diff --no-index --ignore-all-space --color-moved dist/${js}d.ts ../wikiparser-node/${js}d.ts
+	fi
 fi
-git diff --no-index --ignore-all-space $src ../wikiparser-node/$js
