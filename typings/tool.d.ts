29160531b724da509ed1e62b6a0f94d98ecfd20f
Vi-brance <vi_brance@outlook.com>
2023-08-10 10:50:40 +0800

    add typings

---
+++ typings/tool.d.ts
@@ -0,0 +1,6 @@
+import { Token } from "../src";
+import AstText from "../lib/text";
+
+export interface CollectionCallback<T, S> extends Function {
+  call: (thisArg: AstText | Token, i: number, ele: S) => T;
+}
